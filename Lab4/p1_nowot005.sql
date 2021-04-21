eab95490c3a8e4b81d2dde34ca5220a67e5bb874
Henry Sotsaikich <sotsa001@umn.edu>
2021-04-21 12:54:09 -0500

    comments

--- Lab4/p1_nowot005.sql
+++ Lab4/p1_nowot005.sql
@@ -1,6 +1,6 @@
-\i /home/nowot005/CSCI5708labs/Lab4/data_set/department.sql
-\i /home/nowot005/CSCI5708labs/Lab4/data_set/major.sql
-\i /home/nowot005/CSCI5708labs/Lab4/data_set/student.sql
+\i /home/sotsa001/Desktop/CSCI5708labs/Lab4/data_set/department.sql
+\i /home/sotsa001/Desktop/CSCI5708labs/Lab4/data_set/major.sql
+\i /home/sotsa001/Desktop/CSCI5708labs/Lab4/data_set/student.sql
 \timing
 
 -- QUESTION 1 --
@@ -11,13 +11,24 @@ WHERE S.age > 25;
 
 SELECT S.sname
 FROM Student S
-WHERE S.age > 20;
-
+WHERE S.age = 20;
 
 /*
 Query 1 Time: 121.579 ms
 Query 2 Time: 129.184 ms
+
+Query 1 Time: 74.055 ms
+Query 2 Time: 25.518 ms
 */
+
+/*
+Query 1 is executed by searching the heap for all pages with student records that have an age>25.
+This range search would often cost BD.
+
+Query 2 is executed by searching the heap for all pages with student records that have an age of 20.
+This is an equality search that would cost 0.5BD.
+*/
+
 
 -- QUESTION 2 --
 
@@ -29,12 +40,24 @@ WHERE S.age > 25;
 
 SELECT S.sname
 FROM Student S
-WHERE S.age > 20;
-
+WHERE S.age = 20;
 
 /*
 Query 1 Time: 117.347 ms
 Query 2 Time: 118.445 ms
+
+Query 1 Time: 72.505 ms
+Query 2 Time: 5.562 ms
+*/
+
+/*
+Query 1 is relatively the same to Q(1) since both would cost around BD for range searches. Since hash indexes perform well for equality searches, Query 2 has a much lower execution time than Q(1).
+
+Query 1 is executed by using age>25 as a hash key to search for the pages with student records that have age>25. 
+This range search on a hash costs BD
+
+Query 2 is executed by using the age 20 as a hash key to search for the pages with student records that have an age of 20.
+This equality search using a hash costs 2D.
 */
 
 -- QUESTION 3 --
@@ -47,10 +70,25 @@ WHERE S.age > 25;
 
 SELECT S.sname
 FROM Student S
-WHERE S.age >20;
+WHERE S.age = 20;
+
 /*
 Query 1 Time: 127.907 ms
 Query 2 Time: 142.960 ms
+
+Query 1 Time: 79.186 ms
+Query 2 Time: 5.856 ms
+*/
+
+/*
+Query 1 compared to Q(1) and Q(2) runs slightly slower when using range search.
+Similar to Q(2), query 2 has a fast runtime for b+ trees and performs just as well as hash indexes. Like Q(2), this query is much faster than Q(1).
+
+Query 1 is executed by traversing a balanced b+ tree for the leaf nodes with keys that satisfy the condition age>25 and then using the rowid provided to find the records stored.
+Ranges searches on b+ trees cost DlogF(1.5B+# matching pages).
+
+Query 2 is executed by traversing a balanced b+ tree for the leaf node that has a key with 25 and using the rowid to find where the record is stored.
+An equality search on a b+ tree would cost DlogF(1.5B).
 */
 
 -- QUESTION 4 --
@@ -63,15 +101,38 @@ WHERE S.age > 25;
 
 SELECT S.sname
 FROM Student S
-WHERE S.age >20;
+WHERE S.age = 20;
+
 /*
 Query 1 Time: 126.976 ms
 Query 2 Time: 145.595 ms
+
+Query 1 Time: 73.800 ms
+Query 2 Time: 5.805 ms
 */
-\timing
+
+/*
+Query 1 takes time equal to both Q(1) and Q(2) with Q(3) being very slighly slower.
+Query 2 runs in a similar time to both Q(2) and Q(3). Its similarities with Q(3) come from the secondary index on the complex key not being used so we are searching using only age just like Q(3).
+
+Query 1 is executed by traversing a balanced b+ tree for the leaf nodes with keys that satisfy the condition age>25 and then using the rowid provided to find the records stored. The second part of the complex key is not important to the where clause so it has no affect on the runtime. Just like Q(3), range searches on b+ trees cost DlogF(1.5B+# matching pages). 
+
+Query 2 is executed by traversing a balanced b+ tree for the leaf node that has a key with 25 and using the rowid to find where the record is stored.
+Since we search for all students age 20, the secondary ordering of pages by sname has no impact. An equality search on a b+ tree would cost DlogF(1.5B).
+*/ 
 -- QUESTION 5 --
+
 SELECT S.sname
 FROM Student S 
 WHERE S.age = 0;
 
+/*
+Time: 0.451 ms
+*/
+
+/*
+This query would not use any of the indexes because there are no records with age 0 in the database.
+*/
+
+\timing
 -- Does not use the index since the age of 0 is not present anywhere in the student data file.
--- Lab4/p2_nowot005.sql
+++ Lab4/p2_nowot005.sql
@@ -19,6 +19,12 @@ Query 1 Time: 55.755 ms
 Query 2 Time: 113.744 ms
 */
 
+/*
+Query 1 is executed around half the time of query 2.
+Query 2 is slower than query 1 because of the key word DISTINCT which causes 
+the database to sort the queries again to determine if there are dublicates within the query.
+*/
+
 -- QUESTION 2 --
 
 SELECT sid
@@ -35,6 +41,7 @@ HAVING year = 4;
 Query 1 Time: 68.940 ms
 Query 2 Time: 70.870 ms
 */
+
 
 -- QUESTION 3 --
 
@@ -55,6 +62,11 @@ GROUP BY dname;
 /*
 Query 1 Time: 402.982 ms
 Query 2 Time: 428.397 ms
+*/
+
+/*
+They have relatively similar execution times.
+The slight difference in query 1 vs query 2 may be because query 2 will sequentially scan every record sid from major and then scan student for matching sid before the selection age<30 rather than selecting the students with age<30 from student and then scanning it and major for matching sid. 
 */
 
 \timing
